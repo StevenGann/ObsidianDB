a95df6bb710e62de009b976bda7b4541ae1eafcb
Steven Gann <stevengannjr@gmail.com>
2025-04-24 10:42:28 -0700

    Improvements to example program

--- ObsidianDB.Example/Program.cs
+++ ObsidianDB.Example/Program.cs
@@ -28,6 +28,6 @@ while(true)
     db.Update();
     Thread.Sleep(1000);
 
-    Note n = db.GetFromId("59d9b9f0-73bd-430e-b0c5-2725c66cfbfa");
-    Console.WriteLine(n.Hash);
+    //Note n = db.GetFromId("59d9b9f0-73bd-430e-b0c5-2725c66cfbfa");
+    //Console.WriteLine(n.Hash);
 }
